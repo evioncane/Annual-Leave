420b61da4ddcaee743c90eea9caa2247f749cc23
evioncane <evioncane@gmail.com>
2020-10-05 21:25:41 +0200

    Fix of module structure

--- service-layer/src/test/java/com/example/demo/AnnualLeaveApplicationTests.java
+++ service-layer/src/test/java/com/example/demo/AnnualLeaveApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.service.TestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnnualLeaveApplicationTests {
 
+	@Autowired
+	TestService testService;
+
 	@Test
 	void contextLoads() {
+		assert testService.greet().equals("Greetings!");
 	}
 
 }
